03c0ad85074dcf2170045a55ab462a5831ff9a68
DESKTOP-4DR04SQ\GG <c1004ds@naver.com>
2024-05-01 15:21:55 +0900

    Revert "no message"
    This reverts commit c3abef7b5e25957902e19dfcec8faf5de39f108d.

---
+++ demo_prof/src/main/java/com/example/demo_prof/Dao/UserInfoDao.java
@@ -0,0 +1,17 @@
+package com.example.demo_prof.Dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo_prof.domain.UserInfoDto;
+
+@Mapper
+public interface UserInfoDao {
+	
+	public UserInfoDto getUserInfo(String uno); 
+	public List<UserInfoDto> getUserInfoList();
+	public int saveUserInfo(UserInfoDto userInfoDto );
+	public int deleteUserInfo(String uno);
+	 
+}
