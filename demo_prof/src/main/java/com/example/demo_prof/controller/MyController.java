e2ef8854a3fbc16aa8e7e6592782d89943185264
Byeon JiEun <ster137@naver.com>
2024-05-01 12:33:22 +0900

    Revert "컨트롤러1"
    This reverts commit b2f36517533ac6f2b2bd0756987c4e88e6894b1e.

--- demo_prof/src/main/java/com/example/demo_prof/controller/MyController.java
+++ demo_prof/src/main/java/com/example/demo_prof/controller/MyController.java
@@ -3,40 +3,19 @@ package com.example.demo_prof.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.log4j.Log4j2;
-
 @Controller
 @Log4j2
+
 public class MyController {
 
 	@RequestMapping("/")
-	public String root() {
-		return "main";
+	public @ResponseBody String root() {
+		return "hi";
 	}
 	
-	@RequestMapping("/list")
-	public String getList() {
-		return "list";
-	}
 	
-	@RequestMapping("/writeForm")
-	public String writeForm() {
-		return "writeForm";
-	}
 	
-	@RequestMapping("/write")
-	public String write() {
-		return "redirect:list";
-	}
-	
-	@RequestMapping("/detail")
-	public String detail() {
-		return "detail";
-	}
-	
-	@RequestMapping("/delete")
-	public String delete() {
-		return "redirect:list";
-	}
 }
