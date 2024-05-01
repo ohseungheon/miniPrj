f285619e8ce35ba86da4cef7a3a365512b651746
osh <oshble5151@gmail.com>
2024-05-01 12:03:15 +0900

    초기설정완료 커밋1

---
+++ demo_prof/src/test/java/com/example/demo_prof/DemoProfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_prof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
